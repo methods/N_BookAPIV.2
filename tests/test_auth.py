bf93c999b5faa7b02fcecaae6298304a22d6e45b
Nicholas.Goh@methods.co.uk <Nicholas.Goh@methods.co.uk>
2025-06-26 09:38:01 +0100

    Add test_auth.py with test_init_oauth, update requirements and makefile.

---
+++ tests/test_auth.py
@@ -0,0 +1,30 @@
+# pylint: disable=missing-docstring
+from unittest.mock import MagicMock
+import pytest
+import auth.services as auth_services
+
+
+def test_init_oauth_calls_authlib_correctly(mocker):
+    """ This test should test *our* code calls the library correctly """
+    # Mock the Authlib functions that will be called by init_oauth
+    mock_init_app = mocker.patch('auth.services.oauth.init_app')
+    mock_register = mocker.patch('auth.services.oauth.register')
+
+    # Mock the Flask app
+    mock_app = MagicMock()
+
+    # Define the expected values from the function call
+    expected_call_args = {
+        'name': 'google',
+        'client_id': auth_services.GOOGLE_CLIENT_ID,
+        'client_secret': auth_services.GOOGLE_CLIENT_SECRET,
+        'server_metadata_url': 'https://accounts.google.com/.well-known/openid-configuration',
+        'client_kwargs': {'scope': 'openid email profile'}
+    }
+
+    # Call the function
+    auth_services.init_oauth(mock_app)
+
+    # Assert outcomes
+    mock_init_app.assert_called_once_with(mock_app)
+    mock_register.assert_called_once_with(**expected_call_args)
