e6553e7415407ccdbd2555fad21d9de67c39b785
Nicholas.Goh@methods.co.uk <Nicholas.Goh@methods.co.uk>
2025-07-07 10:23:44 +0100

    Add minimal code to @login_required decorator for anon user test

--- auth/decorators.py
+++ auth/decorators.py
@@ -1,11 +1,15 @@
 """ Contains all the authorization decorators to control database access """
 from functools import wraps
+from flask import session, redirect
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        # To be implemented
-        pass
+        #
+        if session.get('user_id') is None:
+            return redirect('http://localhost:5000/auth/login')
+
+        return f(*args, **kwargs)
     return decorated_function
 
 def roles_required(*required_roles):
